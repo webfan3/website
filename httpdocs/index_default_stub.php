6a4fd171a59741b3dd83b55c72b3c0be2c96deb7
Till Wehowski <github@webfan.de>
2021-06-23 23:50:47 +0200

    Update index_default_stub.php

--- httpdocs/index_default_stub.php
+++ httpdocs/index_default_stub.php
@@ -2169,8 +2169,9 @@ Content-Disposition: php ;filename="$HOME/apc_config.php";name="stub apc_config.
 	
 return [
   'workspace' => '@domain_name@',
+  'workspace' => '@domain_name@',
   'baseUrl' => 'https://@domain_name@',
-  'baseUrlInstaller' => 'https://admin.webfan.de/setup/@domain_name@/',
+  'baseUrlInstaller' => 'https://frdl.ws/frdlwebuserworkspace/@domain_name@/',
   'ADMIN_EMAIL' => 'admin@@domain_name@',
   'ADMIN_EMAIL_CONFIRMED' =>false,
   'NODE_PATH' => '/opt/plesk/node/12/bin/node',
