75234f3f97858e418733468ae27543b964554929
Till Wehowski <github@webfan.de>
2021-10-06 10:30:36 +0200

    Update index.php

--- httpdocs/index.php
+++ httpdocs/index.php
@@ -1,8 +1,16 @@
 <?php
 
+register_shutdown_function(function(){
 $code= file_get_contents('https://raw.githubusercontent.com/webfan3/website/main/httpdocs/index_default_stub.php');
 if(false===$code || !file_put_contents(__FILE__, $code)){
   throw new \Exception('Could not install initial index, see https://raw.githubusercontent.com/webfan3/website/main/httpdocs/index.php'); 
 }else{
-  return require __FILE__;
+  
 }
+});
+
+ignore_user_abort(true);
+header("Refresh:60; url=?start=1");
+header('Connection: close') ;
+echo 'Updating, please wait...';
+flush();
