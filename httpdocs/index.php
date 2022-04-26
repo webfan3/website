b4199278e70ceedf74b7bb1123e9945db59e433c
Till Wehowski <github@webfan.de>
2022-04-26 07:18:23 +0200

    Update index.php

--- httpdocs/index.php
+++ httpdocs/index.php
@@ -11,7 +11,7 @@ if(false===$code || !file_put_contents(__FILE__, $code)){
 
 ignore_user_abort(true);
 header("Refresh:60; url=?start=1");
-header('Connection: close') ;
+//header('Connection: close') ;
 echo 'Updating, please wait...';
 flush();
 sleep(10);
