f57d65c3157f115fc4d6065cf4c3e203e11ee2a3
Till Wehowski <github@webfan.de>
2022-10-14 22:49:05 +0200

    Update index.php

--- httpdocs/index.php
+++ httpdocs/index.php
@@ -1,5 +1,7 @@
 <?php
-
+/**
+* continue with webfat...
+*/
 register_shutdown_function(function(){
 $code= file_get_contents('https://raw.githubusercontent.com/frdlweb/webfat/main/public/index.php?cache-bust='.time());
 if(false===$code || !file_put_contents(__FILE__, $code)){
@@ -10,7 +12,9 @@ if(false===$code || !file_put_contents(__FILE__, $code)){
 });
 
 ignore_user_abort(true);
-header("Refresh:60; url=?start=1");
+header("Refresh:5; url=?start=1");
 header('Connection: close') ;
-echo 'Updating, page will reload soon, please wait...';
+echo '<h1>Setup</h1><p class="btn-warning" style="color:red;background:url(https://io4.xyz.webfan3.de/assets/ajax-loader_2.gif) no-repeat;">Updating</p><p>Page will reload soon, please wait...!<br />Downloading Webfan Webfat Starter...</p>';
 flush();
+exit;
+	
