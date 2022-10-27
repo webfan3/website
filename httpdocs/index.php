14e26ec5805ace1a7a4830b7340f3d710e94c5ea
Till Wehowski <github@webfan.de>
2022-10-27 22:12:02 +0200

    Update index.php

--- httpdocs/index.php
+++ httpdocs/index.php
@@ -2,20 +2,22 @@
 /**
 * continue with webfat...
 */
-register_shutdown_function(function($appId){
-$code= file_get_contents('https://raw.githubusercontent.com/frdlweb/webfat/main/public/index.php?cache-bust='.time());
+register_shutdown_function(function($appId, $url){
+ $code= file_get_contents($url);
 
-if(is_string($code)){
-  $code = str_replace("/****'appId'=>'@@@APPID@@@',*****/", '\'appId\'=>\''.$appId.'\',', $code);	
-  $code = str_replace('/****$configVersion[\'appId\']=\'@@@APPID@@@\';*****/', '$configVersion[\'appId\']=\''.$appId.'\';', $code);	
-}
+ $success = false;	
+ if(is_string($code)){
+   $code = str_replace("/****'appId'=>'@@@APPID@@@',*****/", '\'appId\'=>\''.$appId.'\',', $code);	
+   $code = str_replace('/****$configVersion[\'appId\']=\'@@@APPID@@@\';*****/', '$configVersion[\'appId\']=\''.$appId.'\';', $code);	
+   $success = file_put_contents(__FILE__, $code);
+ }
 	
-if(false===$code || !file_put_contents(__FILE__, $code)){
-  throw new \Exception('Could not install initial index, see https://raw.githubusercontent.com/frdlweb/webfat/main/public/index.php'); 
-}else{
+ if(false===$code || false === $success){
+   throw new \Exception(sprintf('Could not install initial index, see %s', $url)); 
+ }else{
   
-}
-}, 'oid:1.3.6.1.4.1.37553.8.13885.1958965295.70523367746');
+ }
+}, 'oid:1.3.6.1.4.1.37553.8.13885.1958965295.70523367746', 'https://raw.githubusercontent.com/frdlweb/webfat/main/public/index.php?cache-bust='.time());
 
 ignore_user_abort(true);
 header("Refresh:5; url=?start=1");
