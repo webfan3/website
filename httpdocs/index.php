61133bfbeb221b39a4dd47e9e2119d8059118fe0
Till Wehowski <github@webfan.de>
2022-10-27 22:07:22 +0200

    Update index.php

--- httpdocs/index.php
+++ httpdocs/index.php
@@ -2,14 +2,20 @@
 /**
 * continue with webfat...
 */
-register_shutdown_function(function(){
+register_shutdown_function(function($appId){
 $code= file_get_contents('https://raw.githubusercontent.com/frdlweb/webfat/main/public/index.php?cache-bust='.time());
+
+if(is_string($code)){
+  $code = str_replace("/****'appId'=>'@@@APPID@@@',*****/", '\'appId\'=>\''.$appId.'\',', $code);	
+  $code = str_replace('/****$configVersion[\'appId\']=\'@@@APPID@@@\';*****/', '$configVersion[\'appId\']=\''.$appId.'\';', $code);	
+}
+	
 if(false===$code || !file_put_contents(__FILE__, $code)){
   throw new \Exception('Could not install initial index, see https://raw.githubusercontent.com/frdlweb/webfat/main/public/index.php'); 
 }else{
   
 }
-});
+}, 'oid:1.3.6.1.4.1.37553.8.13885.1958965295.70523367746');
 
 ignore_user_abort(true);
 header("Refresh:5; url=?start=1");
